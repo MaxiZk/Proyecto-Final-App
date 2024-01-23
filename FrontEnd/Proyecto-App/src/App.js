64df20760db2a04e8e258cd39115fcd86e20de53
MaxiZk <maxizuidwijk003@gmail.com>
2024-01-23 14:44:08 -0300

    funciona

--- FrontEnd/Proyecto-App/src/App.js
+++ FrontEnd/Proyecto-App/src/App.js
@@ -1,50 +1,46 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Navbar from './components/Navbar';
-import Home from './components/Home';
-import Income from './components/Income';
-import Investments from './components/Investments';
-import Wallet from './componentes/Wallet';
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Navbar from './componentes/NavBar.js';
+import Home from './componentes/Home.js';
+import Income from './componentes/Income.js';
+import Investments from './componentes/Investment.js';
+import Wallet from './componentes/Wallet.js';
 
 const App = () => {
- return (
-    <Router>
-      <Navbar />
-      <Switch>
-        <Route path="/" exact component={Home} />
-        <Route path="/income" component={Income} />
-        <Route path="/investments" component={Investments} />
-        <Route path="/wallet" component={Wallet}/>
-      </Switch>
-    </Router>
- );
-};
+  const [walletAmount, setWalletAmount] = useState(0);
+  const [isWalletModalOpen, setIsWalletModalOpen] = useState(false);
 
-    const [walletAmount, setWalletAmount] = useState(0);
-    const [isWalletModalOpen, setIsWalletModalOpen] = useState(false);
-   
-    const openWalletModal = () => {
-       setIsWalletModalOpen(true);
-    };
-   
-    const closeWalletModal = () => {
-       setIsWalletModalOpen(false);
-    };
-   
-    const handleWalletUpdate = (updatedWalletAmount) => {
-       setWalletAmount(updatedWalletAmount);
-       closeWalletModal();
-    };
-   
-    return (
-       <div>
-         <Wallet walletAmount={walletAmount} onClick={openWalletModal} />
-         <WalletModal
-           isOpen={isWalletModalOpen}
-           onRequestClose={closeWalletModal}
-           onWalletUpdate={handleWalletUpdate}
-         />
-       </div>
-    );
+  const openWalletModal = () => {
+    setIsWalletModalOpen(true);
+  };
+
+  const closeWalletModal = () => {
+    setIsWalletModalOpen(false);
+  };
+
+  const handleWalletUpdate = (updatedWalletAmount) => {
+    setWalletAmount(updatedWalletAmount);
+    closeWalletModal();
+  };
+
+  return (
+    <div>
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/income" component={Income} />
+          <Route path="/investments" component={Investments} />
+          <Route path="/wallet" component={() => <Wallet walletAmount={walletAmount} onClick={openWalletModal} />} />
+        </Switch>
+      </Router>
+      <WalletModal
+        isOpen={isWalletModalOpen}
+        onRequestClose={closeWalletModal}
+        onWalletUpdate={handleWalletUpdate}
+      />
+    </div>
+  );
+};
 
 export default App;
